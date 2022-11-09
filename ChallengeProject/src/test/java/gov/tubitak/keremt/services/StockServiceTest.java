55f2efff5a666de0284783dc58c954d80566c01c
KeremTAN <keremtan.95@hotmail.com>
2022-11-09 09:30:38 +0300

    StockServiceTest is refactored

--- ChallengeProject/src/test/java/gov/tubitak/keremt/services/StockServiceTest.java
+++ ChallengeProject/src/test/java/gov/tubitak/keremt/services/StockServiceTest.java
@@ -9,11 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class StockServiceTest {
@@ -23,65 +23,94 @@ public class StockServiceTest {
     private StockRepository stockRepository;
     @Mock
     private StockConverter stockConverter;
-    List<StockDto> testValue =new LinkedList<>();
-    StockDto testStockDto =new StockDto("2022-09-06","IBM",
+    List<StockDto> testStockDTOList =new LinkedList<>();
+    List<Stock> mockStockList =new LinkedList<>();
+    StockDto testFirstIBMStockDto =new StockDto("2022-09-06","IBM",
             BigDecimal.valueOf(127.80),BigDecimal.valueOf(128.06),BigDecimal.valueOf(126.30),
             BigDecimal.valueOf(126.72), BigDecimal.valueOf(3345343.00));
-    Stock mockStock =new Stock(1L,"2022-09-06","IBM",
+    StockDto testSecondIBMStockDto =new StockDto("2022-08-31","IBM",
+            BigDecimal.valueOf(129.92),BigDecimal.valueOf(130.00),BigDecimal.valueOf(128.40),
+            BigDecimal.valueOf(128.45), BigDecimal.valueOf(3490380.00));
+    Stock mockFirstIBMStock =new Stock(1L,"2022-09-06","IBM",
             BigDecimal.valueOf(127.80),BigDecimal.valueOf(128.06),BigDecimal.valueOf(126.30),
             BigDecimal.valueOf(126.72), BigDecimal.valueOf(3345343.00));
+    Stock mockSecondIBMStock =new Stock(2L, "2022-08-31","IBM",
+            BigDecimal.valueOf(129.92),BigDecimal.valueOf(130.00),BigDecimal.valueOf(128.40),
+            BigDecimal.valueOf(128.45), BigDecimal.valueOf(3490380.00));
 
-    public StockServiceTest() {
-        testValue.add(testStockDto);
+    private void createIBMList(){
+        testStockDTOList.clear();
+        testStockDTOList.add(testFirstIBMStockDto);
+        testStockDTOList.add(testSecondIBMStockDto);
+        mockStockList.clear();
+        mockStockList.add(mockFirstIBMStock);
+        mockStockList.add(mockSecondIBMStock);
     }
-
-
-
-
+    private void createFirstIBMStockDTO(){
+        testStockDTOList.clear();
+        testStockDTOList.add(testFirstIBMStockDto);
+    }
     /** GetPrice Method*/
     @Test
     public void whenGetPriceCalledWithValidRequestTypeOpen_ReturnsOpenPrice(){
-        Mockito.when(stockConverter.convertToStockDto(mockStock)).thenReturn(testStockDto);
-        Mockito.when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
-                .thenReturn(mockStock);
+        createFirstIBMStockDTO();
+        when(stockConverter.convertToStockDto(mockFirstIBMStock))
+                .thenReturn(testFirstIBMStockDto);
+        when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
+                .thenReturn(mockFirstIBMStock);
                                 //testValue.get(0).getHigh()
         Assertions.assertEquals(BigDecimal.valueOf(127.80), service.getPrice("IBM","2022-09-06","open"));
     }
     @Test
     public void whenGetPriceCalledWithValidRequestTypHigh_ReturnsHighPrice(){
-        Mockito.when(stockConverter.convertToStockDto(mockStock)).thenReturn(testStockDto);
-        Mockito.when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
-                .thenReturn(mockStock);
+        createFirstIBMStockDTO();
+        when(stockConverter.convertToStockDto(mockFirstIBMStock))
+                .thenReturn(testFirstIBMStockDto);
+        when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
+                .thenReturn(mockFirstIBMStock);
         Assertions.assertEquals(BigDecimal.valueOf(128.06), service.getPrice("IBM","2022-09-06","high"));
     }
     @Test
     public void whenGetPriceCalledWithValidRequestTypeLow_ReturnsLowPrice(){
-        Mockito.when(stockConverter.convertToStockDto(mockStock)).thenReturn(testStockDto);
-        Mockito.when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
-                .thenReturn(mockStock);
+        createFirstIBMStockDTO();
+        when(stockConverter.convertToStockDto(mockFirstIBMStock))
+                .thenReturn(testFirstIBMStockDto);
+        when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
+                .thenReturn(mockFirstIBMStock);
         Assertions.assertEquals(BigDecimal.valueOf(126.30), service.getPrice("IBM","2022-09-06","low"));
     }
-
     @Test
     public void whenGetPriceCalledWithValidRequestTypeClose_ReturnsClosePrice(){
-        Mockito.when(stockConverter.convertToStockDto(mockStock)).thenReturn(testStockDto);
-        Mockito.when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
-                .thenReturn(mockStock);
+        createFirstIBMStockDTO();
+        when(stockConverter.convertToStockDto(mockFirstIBMStock))
+                .thenReturn(testFirstIBMStockDto);
+        when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
+                .thenReturn(mockFirstIBMStock);
         Assertions.assertEquals(BigDecimal.valueOf(126.72), service.getPrice("IBM","2022-09-06","close"));
     }
-
     @Test
     public void whenGetPriceCalledWithValidRequestTypeVolume_ReturnsVolume(){
-        Mockito.when(stockConverter.convertToStockDto(mockStock)).thenReturn(testStockDto);
-        Mockito.when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
-                .thenReturn(mockStock);
+        createFirstIBMStockDTO();
+        when(stockConverter.convertToStockDto(mockFirstIBMStock))
+                .thenReturn(testFirstIBMStockDto);
+        when(stockRepository.findBySymbolAndDate("IBM","2022-09-06"))
+                .thenReturn(mockFirstIBMStock);
         Assertions.assertEquals(BigDecimal.valueOf(3345343.00), service.getPrice("IBM","2022-09-06","volume"));
     }
-
-
     @Test
     public void whenGetPriceCalledWithValidRequestInValidType_ReturnsMinus1(){
+
         BigDecimal expectedValue = BigDecimal.valueOf(-1);
         Assertions.assertEquals(expectedValue, service.getPrice("IBM","2022-09-06","none"));
     }
+    /**
+    @Test
+    public void whenGetStocksCalledWithSymbol_ReturnsIBMList(){
+        createIBMList();
+        List<StockDto> check = new LinkedList<>();
+        when(stockRepository.findBySymbol("IBM"))
+                .thenReturn(mockStockList);
+        doCallRealMethod().when(stockConverter).convertToAllAsStockDTO(check,mockStockList);
+        Assertions.assertEquals(testStockDTOList, service.getStocks("IBM",null));
+    }*/
 }
